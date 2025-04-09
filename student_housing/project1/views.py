a7fff073c67723dbbfc7bbed5fcca757d9a450bb
Kavya <n572v597@wichita.edu>
2025-04-09 13:05:48 -0500

    Added the new materialize css template for showing the list of rooms by fetching from database using sql select statement and also created the edit_room.html for editing the room options.

--- student_housing/project1/views.py
+++ student_housing/project1/views.py
@@ -37,3 +37,25 @@ def add_room(request):
 
 def room_success(request):
     return render(request, 'room_success.html')
+
+
+
+def edit_room(request, room_id):
+    room = get_object_or_404(Room, pk=room_id)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('room_list')  # Redirect to room list page after saving
+    else:
+        form = RoomForm(instance=room)
+
+    return render(request, 'edit_room.html', {'form': form, 'room': room})
+
+
+def room_list(request):
+    rooms = Room.objects.all()
+    return render(request, 'room_list.html', {'rooms': rooms})
+
+
